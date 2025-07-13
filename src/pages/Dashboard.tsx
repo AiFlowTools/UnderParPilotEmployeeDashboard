b360ecff35f892e6f867ad8a909921fe4f8172c2
AiFlowTools <aiflowtools@gmail.com>
2025-07-13 15:38:05 -0400

    Updated Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,10 @@
 import EmployeeDashboard from './EmployeeDashboard';
+import Orders from './Orders';
+
 
 export default function Dashboard() {
   return <EmployeeDashboard />;
+}
+export default function Dashboard() {
+  return <Orders />;
 }
