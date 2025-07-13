88ab5fdd83b156c5e7c273468222deca4cdb245d
AiFlowTools <aiflowtools@gmail.com>
2025-07-13 15:54:01 -0400

    Refactor Dashboard with React Router and Separate Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'   // ← this line is critical so Tailwind/base styles load
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <App />
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 )
