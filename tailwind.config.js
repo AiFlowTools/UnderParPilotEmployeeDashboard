cadd403b9e4a19f2560a32d233b0476edd3dd917
AiFlowTools <aiflowtools@gmail.com>
2025-07-13 14:59:25 -0400

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,13 +23,6 @@ module.exports = {
       },
       boxShadow: {
         card: '0 8px 20px rgba(0,0,0,0.05)',
-         screens: {
-      sm: '640px',
-      md: '768px',
-      lg: '1024px', // This is the breakpoint used in your code!
-      xl: '1280px',
-      // ...
-    }
       },
     }
   },
